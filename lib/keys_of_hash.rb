47292fdbc9ba9e8d3f5f455c1a5e19d541256565
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-13 23:11:50 -0400

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+    return_array = []
+    arguments.each do |argument|
+       map do |key, value|
+            if argument == value
+                return_array.push(key)
+end
+end
+       end
+return_array
+end
 end
